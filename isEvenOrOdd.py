7b7a4ed1b26c9eb31cd1972be8a7f926ccb2768a
Tauan Santos <itauandejesus@gmail.com>
2025-05-03 20:44:12 -0300

    feat: add function to convert integer to binary using incremental approach
    This function receives an integer and increments until it reaches that value, converting each step to binary. Still experimental and may contain bugs.

--- isEvenOrOdd.py
+++ isEvenOrOdd.py
@@ -18,6 +18,57 @@ def is_even_or_odd_using_binary(number):
     else:
         print("Is odd")
 
+# OLD CODE
 
-is_even_or_odd(2)
-is_even_or_odd_using_binary(2)
+
+def is_even_or_odd_using_binary2(number):
+    tamanho = [0, 0, 0, 0]
+
+    for i in range(number):
+        for m in tamanho:
+            if (m == 0):
+                tamanho[m] = 1
+            elif (m > 1):
+                tamanho[m] = 0
+                tamanho[m+1] = 1
+            else:
+                tamanho[m] = 0
+
+    print(tamanho)
+
+# NEW CODE
+
+
+def is_even_or_odd_using_binary3(number):
+    mylist = [0]
+    size = len(mylist)
+    cont = 0
+
+    if (number == 1):
+        mylist[0] = 1
+        return False
+
+    for i in range(number):
+        for m in range(len(mylist)):
+            if (mylist[m] == 0):
+                mylist[m] = 1
+                cont = cont+1
+            elif (mylist[m] == 1 and mylist[0] == 1):
+                mylist[m] = 0
+                cont = cont+1
+                if (cont == number):
+                    mylist.append(1)
+
+    mylist.reverse
+    for i in mylist:
+        print(mylist)
+    if (mylist[size-1] == 0):
+        return True
+    else:
+        return False
+
+
+is_even_or_odd(3)
+is_even_or_odd_using_binary(3)
+is_even_or_odd_using_binary2(3)
+is_even_or_odd_using_binary3(3)
